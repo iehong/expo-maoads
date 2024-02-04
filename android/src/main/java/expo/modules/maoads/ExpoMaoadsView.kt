bdd27b678af9f6ae8f71a5218b30d313756926a3
iehong <iehong@qq.com>
2024-02-04 10:13:45 +0800

    fix:开屏广告 激励视频

--- android/src/main/java/expo/modules/maoads/ExpoMaoadsView.kt
+++ android/src/main/java/expo/modules/maoads/ExpoMaoadsView.kt
@@ -2,15 +2,6 @@ package expo.modules.maoads
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.util.Log
-import android.view.View
-import android.view.ViewGroup
-import android.widget.FrameLayout
-import androidx.fragment.app.Fragment
-import com.sjm.sjmsdk.ad.SjmAdError
-import com.sjm.sjmsdk.ad.SjmBannerAd
-import com.sjm.sjmsdk.ad.SjmBannerAdListener
-import com.sjm.sjmsdk.ad.SjmRewardVideoAdListener
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.views.ExpoView
 
@@ -18,42 +9,6 @@ import expo.modules.kotlin.views.ExpoView
 class ExpoMaoadsView(context: Context, appContext: AppContext) : ExpoView(context, appContext) {
 
   internal var adsId: String = "sjmad_test003"
-  private var bannerAd: SjmBannerAd? = null;
-
-  private val f = FrameLayout(context).also {
-    it.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
-    Log.d("ExpoMaoadsView", adsId);
-    bannerAd = SjmBannerAd(
-      appContext.currentActivity,
-      adsId,
-      object : SjmBannerAdListener {
-        override fun onSjmAdLoaded() {
-          Log.d("ExpoMaoadsView", "onSjmAdLoaded");
-        }
-
-        override fun onSjmAdShow() {
-          Log.d("ExpoMaoadsView", "onSjmAdShow");
-        }
-
-        override fun onSjmAdClicked() {
-          Log.d("ExpoMaoadsView", "onSjmAdClicked");
-        }
-
-        override fun onSjmAdClosed() {
-          Log.d("ExpoMaoadsView", "onSjmAdClosed");
-        }
-
-        override fun onSjmAdError(p0: SjmAdError?) {
-          if (p0 != null) {
-            Log.d("ExpoMaoadsView", "onSjmAdError" + p0.errorMsg)
-          };
-        }
-      }
-    )
-    bannerAd!!.setBannerContainer(it);
-    Log.d("ExpoMaoadsView", adsId);
-    bannerAd!!.loadAD()
-    addView(it)
-  }
+//  private var bannerAd: SjmBannerAd? = null;
 
 }
